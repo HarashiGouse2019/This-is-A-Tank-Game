8111d2610204296951a5e89a7280b53ea81dce5b
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-06-03 15:14:05 -0600

    Tank Update!!!!

---
+++ Assets/Scripts/Miscellaneous/AIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public TankData pawn;
+
+    public void AISeekUpdate(Transform target)
+    {
+        Vector3 targetVector = (target.position - pawn.transform.position).normalized;
+        //pawn.mover.RotateTowards(targetVector);
+        pawn.mover.Move(Vector3.forward);
+    }
+    public void AIFlee(Transform target)
+    {
+        Vector3 targetVector = (target.position - pawn.transform.position);
+        Vector3 awayVector = -targetVector;
+        //pawn.mover.rotateTowards(awayVector);
+        pawn.mover.Move(Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/Miscellaneous/PatrolAIController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolAIController : AIController
+{
+    public enum LoopType { Loop, Stop, PingPong, Random};
+    public List<Transform> wayPoints;
+    public int currentWayPoint;
+    public float cutoff;
+    public bool isForward;
+
+    public LoopType looptype;
+
+    private void Start()
+    {
+        currentWayPoint = 0;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Patrol()
+    {
+        AISeekUpdate(wayPoints[currentWayPoint]);
+        if (Vector3.Distance(pawn.transform.position, wayPoints[currentWayPoint].position) <= cutoff)
+        {
+            if (isForward) {
+                currentWayPoint++;
+            } else {
+                currentWayPoint--;
+            }
+            
+
+            if (currentWayPoint >= wayPoints.Count || currentWayPoint < 0)
+            {
+                if (looptype == LoopType.Loop)
+                {
+                    currentWayPoint = 0;
+
+                } else if (looptype == LoopType.Random)
+                {
+                    currentWayPoint = Random.Range(0, wayPoints.Count);
+                } else if (looptype == LoopType.PingPong)
+                {
+                    isForward = !isForward;
+                    if (currentWayPoint >= wayPoints.Count)
+                    {
+                        currentWayPoint = wayPoints.Count - 1;
+                    } else
+                    {
+                        currentWayPoint = 0;
+                    }
+                }
+            }
+        }
+    }
+}
